4c296433262f4ff7c600aafb92c40d6082926f54
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-08-07 15:46:58 -0400

    added basic logic to determine wheter or not the type is included.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,15 @@
 module Lib where
 
+isLowerCase :: Char -> Bool
+isLowerCase char = char `elem` lowerCaseAlphabet
+
+isUpper :: Char -> Bool
+isUpper char = char `elem` upperCaseAlphabet
+
+isDigit :: Int -> Bool
+isDigit digi = digi `elem` digits
+
+
 type Alphabet = [Char] 
 lowerCaseAlphabet :: Alphabet
 lowerCaseAlphabet = ['a'.. 'z']
