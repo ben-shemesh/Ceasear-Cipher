31266dbe90167c6290274aefe1e14129fdcd221c
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-08-07 22:09:13 -0400

    added helper functions that can be used to check char of cipher.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,5 @@
 module Lib where
 
-isLowerCase :: Char -> Bool
-isLowerCase char = char `elem` lowerCaseAlphabet
-
-isUpper :: Char -> Bool
-isUpper char = char `elem` upperCaseAlphabet
-
-isDigit :: Int -> Bool
-isDigit digi = digi `elem` digits
-
-
 type Alphabet = [Char] 
 lowerCaseAlphabet :: Alphabet
 lowerCaseAlphabet = ['a'.. 'z']
@@ -19,3 +9,15 @@ upperCaseAlphabet = ['A' .. 'Z']
 
 digits :: Alphabet
 digits = ['0' .. '9']
+
+isLowerCase :: Char -> Bool
+isLowerCase char = char `elem` lowerCaseAlphabet
+
+isUpperCase :: Char -> Bool
+isUpperCase char = char `elem` upperCaseAlphabet
+
+isDigit :: Char -> Bool
+isDigit char = char `elem` digits
+
+isMisc :: Char -> Bool
+isMisc char = char `notElem` lowerCaseAlphabet ++ upperCaseAlphabet ++ digits
